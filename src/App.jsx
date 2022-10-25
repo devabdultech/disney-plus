1e857d497dfc53c55c5b793bad57f8c71dd62256
Abdulhameed735 <abdulhameedmoonstapha735@gmail.com>
2022-10-25 21:47:48 +0100

    Code Splited and added a cool loader animation when fetching Javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom'
+import { DotLoader } from 'react-spinners';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import MoviePage from './pages/MoviePage';
 import MovieDetails from './pages/MovieDetails';
 import NoMatch from './pages/NoMatch';
+
+const LoginPage = lazy(() => import('./pages/LoginPage'))
+const SignupPage = lazy(() => import('./pages/SignupPage'))
+const MoviePage = lazy(() => import('./pages/MoviePage'))
 
 function App() {
 
   return (
     <div className="App text-white font-regular">
+      <Suspense fallback={
+        <DotLoader
+          className='w-full mx-auto mt-36'
+          color="#36d7b7"  
+          size={65}
+          speedMultiplier={2} 
+        />
+        }
+      >
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='login' element={<LoginPage />} />
@@ -18,6 +30,7 @@ function App() {
           <Route path='details/:kind/:id' element={<MovieDetails />} />
           <Route path='*' element={<NoMatch />} />
         </Routes>
+      </Suspense>
     </div>
   )
 }
